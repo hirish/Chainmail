38aca3171d82695a390c09328ad3b5042b1b3fc5
hirish <me@henryirish.com>
2013-04-06 13:22:44 +0100

    Adding SSL MITM Support.
    We now can intercept SSL connections. We start an SSL client connection
    with the server, as well as acting like an SSL server to the client.
    
    We can now sniff and alter HTTPS traffic.
    However, this (obviously) generates a client-side warning. The Java
    application will have to add the appropriate certificates to Android.

--- Connection.py
+++ Connection.py
@@ -1,16 +1,25 @@
 import socket
+import ssl
 from HTTP import HTTP_Message
 
-class Connection:
+class HTTPConnection:
 
 	def __init__(self, client):
 		self.client = client
 		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+		self.client_name = client.getpeername()
+		self.server_name = "Server Unconnected"
+
+	def __str__(self):
+		return "<Connection between %s and %s>" % (self.client_name,
+												   self.server_name)
 	
 	def connect_server(self, server_host, port = 80):
 		try:
 			# Connect the server to the specified host.
 			self.server.connect((server_host, port))
+			self.server_name = self.server.getpeername()
 			return True
 		except Exception as e:
 			print e
@@ -52,3 +61,17 @@ class Connection:
 		print ">>>>>>>>>>>>>>"
 		print message_data.reform()
 		print ">>>>>>>>>>>>>>"
+
+class HTTPSConnection(HTTPConnection):
+
+	def __init__(self, client):
+		self.client = client
+		unwrapped_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.server = ssl.wrap_socket(unwrapped_socket)
+
+		self.client_name = client.getpeername()
+		self.server_name = "Server Unconnected"
+
+	# Change the default port to connect to.
+	def connect_server(self, server_host, port = 443):
+		super(HTTPConnection, self).connect_server(server_host, port)
--- ProxyServer.py
+++ ProxyServer.py
@@ -1,11 +1,14 @@
 import socket
 import select
 import time
+import ssl
 from Headers import Headers
-from Connection import Connection
+from Connection import HTTPConnection, HTTPSConnection
 
 buffer_size = 8192
 delay = 0.0001
+
+message = 'HTTP/1.1 200 Connection established\nProxy-agent: ChainMail/1.0\n\n'
 
 class ProxyServer:
 	input_list = []
@@ -17,6 +20,7 @@ class ProxyServer:
 		self.proxy_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.proxy_socket.bind((host, port))
 		self.proxy_socket.listen(200)
+		print "Listening on ", self.proxy_socket
 
 	def main_loop(self):
 		self.input_list.append(self.proxy_socket)
@@ -28,7 +32,19 @@ class ProxyServer:
 					self.accept()
 					break
 
-				self.data = socket.recv(buffer_size)
+				try:
+					self.data = socket.recv(buffer_size)
+				except Exception:
+					print "Connection Error:"
+					connection = self.connections[socket]
+					if socket == connection.client:
+						print "\t Reset by client."
+					elif socket == connection.server:
+						print "\t Reset by server."
+					else:
+						print "\t Broken."
+					print "\t%s" % connection
+					exit()
 				if len(self.data) == 0:
 					self.close(socket)
 				else:
@@ -48,35 +64,57 @@ class ProxyServer:
 			host = headers.headers['Request']['host'].split('/')[0]
 			port = headers.headers['Request']['port']
 		except KeyError:
-			print ""
-			print "KeyError!"
-			print ""
-			print request
+			print "\nKeyError!\n", request
 			exit()
 
-		# Connect to the remote server we're proxying to.
-		connection = Connection(client_socket)
-		# Success - register the sockets for listening, as well as
-		# the connection
-		if connection.connect_server(host, port):
-			print "Connection between %s and %s:%s established" % (clientaddr, host, str(port))
-			self.input_list.append(connection.client)
-			self.input_list.append(connection.server)
-			self.connections[connection.client] = connection
-			self.connections[connection.server] = connection
-			if headers.headers['Request']['method'] == "CONNECT":
-				message = 'HTTP/1.1 200 Connection established\nProxy-agent: ChainMail/1.0\n\n'
-				print "<<<<<<<<<<<<<<"
-				print message
-				print "<<<<<<<<<<<<<<"
-				client_socket.send(message)
+		# CONNECT = a HTTPS connection.
+		if headers.headers['Request']['method'] == "CONNECT":
+			print "<<<<<<<<<<<<<<\n%s\n<<<<<<<<<<<<<<" % message
+			client_socket.send(message)
+			ssl_client_socket = ssl.wrap_socket(client_socket,
+											   keyfile = "../Certificates/Chainmail.key",
+											   certfile = "../Certificates/Chainmail.crt",
+											   server_side = True,
+											   do_handshake_on_connect = False)
+			ssl_client_socket.do_handshake()
+			# Connect to the remote server we're proxying to.
+			connection = HTTPSConnection(ssl_client_socket)
+			# Success - register the sockets for listening, as well as
+			# the connection
+			if connection.connect_server(host, port):
+				print "Secure connection between %s and %s:%s established\n" \
+					  % (clientaddr, host, str(port)), request
+				self.register_connection(connection)
+			# Failure - close the client connection and display error message.
 			else:
-				connection.send_data(client_socket, request)
-		# Failure - close the client connection and display error message.
+				connection_failure(connection, clientaddr, host)
+
+		# A HTTP connection.
 		else:
-			print "Can't establish connection with server %s." % host
-			print "Closing connection with client %s." % str(clientaddr)
-			connection.client.close()
+			# Connect to the remote server we're proxying to.
+			connection = HTTPConnection(client_socket)
+			# Success - register the sockets for listening, as well as
+			# the connection
+			if connection.connect_server(host, port):
+				print "Connection between %s and %s:%s established\n" \
+					  % (clientaddr, host, str(port)), request
+				self.register_connection(connection)
+				connection.send_data(client_socket, request)
+			# Failure - close the client connection and display error message.
+			else:
+				connection_failure(connection, clientaddr, host)
+	
+	def register_connection(self, connection):
+		self.input_list.append(connection.client)
+		self.input_list.append(connection.server)
+		self.connections[connection.client] = connection
+		self.connections[connection.server] = connection
+
+	def connection_failure(self, connection, clientaddr, host):
+		print "Can't establish secure connection with server %s." % host
+		print "Closing connection with client %s." % str(clientaddr)
+		connection.client.close()
+
 
 	def close(self, socket):
 		print socket.getpeername(), "has disconnected"
