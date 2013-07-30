1fafa82908c2c4ac3685f21c2a59adb73912a73e
hirish <me@henryirish.com>
2013-07-30 22:57:52 +0100

    Logger now outputs timestamps.

--- Logger.py
+++ Logger.py
@@ -1,4 +1,5 @@
 from termcolor import colored
+import time
 
 FILE_LOGGING = True
 
@@ -9,6 +10,13 @@ if FILE_LOGGING:
         log_file = open("log", "w+")
 
 def printer(messages, colour):
+    now = time.asctime( time.localtime(time.time()) )
+    now_string = "%s: %s\n" % (now, colour)
+
+    if FILE_LOGGING:
+        log_file.write(now_string)
+    print colored(now_string, colour), 
+
     for message in messages:
         message = str(message).replace("\r", "")
         print colored(message, colour),
