c408b700a1a711531e0ff991f7bdf4ff29910754
hirish <me@henryirish.com>
2013-07-31 22:17:59 +0100

    Moved Logger from class to static class.
    This means that it can be called from multiple locations utilising the
    same log files. We can also utilise the same (simple) API that I've been
    using for a while.

--- Logger.py
+++ Logger.py
@@ -1,5 +1,5 @@
-from termcolor import colored
 import time
+from termcolor import colored
 
 LEVELS = [
     {'name': 'ERROR', 'colour': 'red'},
@@ -14,69 +14,69 @@ HEADER_COLOUR = 'grey'
 
 
 class Logger:
-
     file_logging = True
     console_logging = True
     output_level = 5
 
     log_file = None
 
-    def __init__(self, file_log=True, console_log=True, output_level=5):
-        self.file_logging = file_log
-        self.console_logging = console_log
-        self.output_level = output_level
 
-        self.get_log_file()
+def file_printer(messages):
+    if not Logger.log_file:
+        Logger.log_file = open("log", "w+")
 
-    def get_log_file(self):
-        if self.file_logging:
-            self.log_file = open("log", "w+")
+    for message in messages:
+        Logger.log_file.write(message)
+    Logger.log_file.write("\n")
 
-    def file_printer(self, messages):
-        for message in messages:
-            self.log_file.write(message)
-        self.log_file.write("\n")
 
-    def console_printer(self, coloured_messages):
-        for coloured_message in coloured_messages:
-            message, colour = coloured_message
-            print colored(message, colour),
-        print ""  # Trailing new line.
+def console_printer(coloured_messages):
+    for coloured_message in coloured_messages:
+        message, colour = coloured_message
+        print colored(message, colour),
+    print ""  # Trailing new line.
 
-    def printer(self, messages, level):
-        if level <= self.output_level:
-            now = time.asctime(time.localtime(time.time()))
-            header_string = "%s: %s\n" % (now, LEVELS[level]['name'])
 
-            headered_messages = [header_string] + messages
+def printer(messages, level):
+    if level <= Logger.output_level:
+        now = time.asctime(time.localtime(time.time()))
+        header_string = "%s: %s\n" % (now, LEVELS[level]['name'])
 
-            if self.file_logging:
-                self.file_printer(headered_messages)
+        headered_messages = [header_string] + list(messages)
 
-            if self.console_logging:
-                colour = LEVELS[level]['colour']
+        if Logger.file_logging:
+            file_printer(headered_messages)
 
-                coloured_header_string = (header_string, HEADER_COLOUR)
-                coloured_messages = zip(messages, [colour] * len(messages))
-                coloured_headered_messages = ([coloured_header_string]
-                                              + coloured_messages)
+        if Logger.console_logging:
+            colour = LEVELS[level]['colour']
 
-                self.console_printer(coloured_headered_messages)
+            coloured_header_string = (header_string, HEADER_COLOUR)
+            coloured_messages = zip(messages, [colour] * len(messages))
+            coloured_headered_messages = ([coloured_header_string]
+                                          + coloured_messages)
 
-    def e(self, *messages):
-        self.printer(messages, 0)
+            console_printer(coloured_headered_messages)
 
-    def w(self, *messages):
-        self.printer(messages, 1)
 
-    def i(self, *messages):
-        self.printer(messages, 2)
+def e(*messages):
+    printer(messages, 0)
 
-    def d(self, *messages):
-        self.printer(messages, 3)
 
-    def v(self, *messages):
-        self.printer(messages, 4)
+def w(*messages):
+    printer(messages, 1)
 
-    def vv(self, *messages):
-        self.printer(messages, 5)
+
+def i(*messages):
+    printer(messages, 2)
+
+
+def d(*messages):
+    printer(messages, 3)
+
+
+def v(*messages):
+    printer(messages, 4)
+
+
+def vv(*messages):
+    printer(messages, 5)
